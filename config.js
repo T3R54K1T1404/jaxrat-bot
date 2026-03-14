00cd48a83b7296751e732cbd4f9687b4116bbfda
T3R54K1T1404 <44319677+T3R54K1T1404@users.noreply.github.com>
2026-03-15 05:09:24 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,32 @@
+module.exports = {
+    // Bot Configuration
+    botToken: "8019851816:AAFytb4-6Owqdttdhja77wqabJt8p_ZUaho",
+    
+    // Owner Configuration
+    ownerChatId: "6754308724", // Chat ID owner @deakfta
+    ownerUsername: "fx",
+    
+    // Payment Configuration
+    paymentPrice: 50000, // 50k in IDR
+    paymentMethods: ["QRIS", "Bank Transfer"],
+    
+    // Firebase Configuration
+    firebaseConfig: {
+        apiKey: "AIzaSyDzA92c3IoyjhiFft3Ci6cpsvEJBXAzIfQ",
+        authDomain: "jaxrat-6813c.firebaseapp.com",
+        databaseURL: "https://jaxrat-6813c-default-rtdb.firebaseio.com",
+        projectId: "jaxrat-6813c",
+        storageBucket: "jaxrat-6813c.firebasestorage.app",
+        messagingSenderId: "750985295231",
+        appId: "1:750985295231:web:0679d4b998e52256e4d64a"
+    },
+    
+    // Group Configuration
+    groupLink: "https://t.me/pangeranjendral",
+    
+    // Admin Configuration
+    admins: ["6754308724"], // Chat IDs of admins
+    
+    // Price for access
+    accessPrice: 50000
+};
